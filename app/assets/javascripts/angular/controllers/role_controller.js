0d8f40c515475b2cf5b6f2368ee05d8bbe37fa97
Erik Nygren <erik@farmdrop.co.uk>
2015-10-18 20:55:56 +0100

    Adding angular-ui calendar directive for preferences form

--- app/assets/javascripts/angular/controllers/role_controller.js
+++ app/assets/javascripts/angular/controllers/role_controller.js
@@ -12,9 +12,18 @@
       console.log(role);
     };
 
+    this.openCalendar = function($event) {
+      this.calendarStatus.opened = true;
+    };
+
     //mocking an array of the 3 user roles
     this.roles = [{}, {}, {}];
     this.showRole(0);
+    this.calendarStatus = {
+      opened: false
+    };
+    this.availableMinDate = new Date();
+    this.availableMaxDate = new Date(2020, 12, 31);
   };
 
   RoleController.$inject = [];
