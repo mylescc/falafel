e5d8d575ad751f169e4570ea95e2f9c8475a5954
Erik Nygren <erik@farmdrop.co.uk>
2015-10-18 18:01:28 +0100

    some visual feedback when hovering between roles, DRYing up some role selection code

--- app/assets/javascripts/angular/controllers/role_controller.js
+++ app/assets/javascripts/angular/controllers/role_controller.js
@@ -5,7 +5,12 @@
     this.showRole = function(index) {
       console.log('showing role ' + (index + 1));
       this.currentRole = this.roles[index];
-    }
+    };
+
+    this.submitRole = function(role) {
+      console.log('submit role called');
+      console.log(role);
+    };
 
     //mocking an array of the 3 user roles
     this.roles = [{}, {}, {}];
