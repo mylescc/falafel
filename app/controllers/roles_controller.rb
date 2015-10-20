f68957cd36fefc65e391a53f942157ad851abde3
Erik Nygren <erik@farmdrop.co.uk>
2015-10-20 23:03:29 +0100

    Exposing Roles from rails controller to AngularJS

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -2,7 +2,13 @@ class RolesController < ApplicationController
   layout 'talent'
 
   def new
-    @role = Role.new(user: current_user)
+    #just mocking sending down the three user
+    # roles in the laziest way i could think of
+    @roles = [
+      Role.new(user: current_user, min_salary: 60000, min_day_rate: 550),
+      Role.new(user: current_user, min_salary: 40000, min_day_rate: 450),
+      Role.new(user: current_user, min_salary: 20000, min_day_rate: 250)
+    ]
   end
 
   def create
