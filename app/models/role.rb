4aa7d131363e338e784236a9309ec7a99da6c3e8
radicalise <carlosjmtz@gmail.com>
2015-10-22 21:44:10 +0100

    stupid

--- app/models/role.rb
+++ app/models/role.rb
@@ -11,11 +11,11 @@ class Role < ActiveRecord::Base
 
   belongs_to :permanent_role_travel_willingness_option, 
           class_name: 'RoleTravelWillingnessOption',
-          foreign_key: 'contract_role_travel_willingness_option_id'
+          foreign_key: 'permanent_role_travel_willingness_option_id'
 
   belongs_to :contract_role_travel_willingness_option, 
           class_name: 'RoleTravelWillingnessOption',
-          foreign_key: 'permanent_role_travel_willingness_option_id'
+          foreign_key: 'contract_role_travel_willingness_option_id'
 
   validates_presence_of :user
 end
