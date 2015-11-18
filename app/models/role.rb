b439756357f7f25080652d902799607ed5fd3b80
Erik Nygren <erik@farmdrop.co.uk>
2015-11-18 21:58:49 +0000

    Adding role position enum

--- app/models/role.rb
+++ app/models/role.rb
@@ -17,5 +17,31 @@ class Role < ActiveRecord::Base
           class_name: 'RoleTravelWillingnessOption',
           foreign_key: 'contract_role_travel_willingness_option_id'
 
-  validates_presence_of :user
+  validates_presence_of :user, :position
+
+  enum position: {
+    'Architect': 1,
+    'Business Analyst': 2,
+    'Developer': 3,
+    'Development Manager': 4,
+    'Graduate': 5,
+    'Head of Architecture': 6,
+    'Head of Development': 7,
+    'Head of Operations': 8,
+    'Head of Service Delivery': 9,
+    'IT Consultant': 10,
+    'IT Director/CIO': 11,
+    'IT Operations Manager': 12,
+    'IT Support Analyst': 13,
+    'IT Trainer': 14,
+    'PMO Analyst': 15,
+    'PMO Manager': 16,
+    'Programme Manager': 17,
+    'Project Manager': 18,
+    'Service Delivery Manager': 19,
+    'Service Desk Analyst': 20,
+    'Service Desk Manager': 21,
+    'Test Analyst': 22,
+    'Test Manager': 23
+  }
 end
