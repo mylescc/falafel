b898d8432de0cd7a912afc088ba46bdbc1581ead
Myles Cowper-Coles <myles@farmdrop.co.uk>
2015-11-18 21:04:37 +0000

    WIP

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,19 +1,19 @@
 class Role < ActiveRecord::Base
   belongs_to :user
 
-  belongs_to :permanent_role_distance, 
+  belongs_to :permanent_role_distance,
           class_name: 'RoleDistance',
           foreign_key: 'permanent_role_distance_id'
 
-  belongs_to :contract_role_distance, 
+  belongs_to :contract_role_distance,
           class_name: 'RoleDistance',
           foreign_key: 'contract_role_distance_id'
 
-  belongs_to :permanent_role_travel_willingness_option, 
+  belongs_to :permanent_role_travel_willingness_option,
           class_name: 'RoleTravelWillingnessOption',
           foreign_key: 'permanent_role_travel_willingness_option_id'
 
-  belongs_to :contract_role_travel_willingness_option, 
+  belongs_to :contract_role_travel_willingness_option,
           class_name: 'RoleTravelWillingnessOption',
           foreign_key: 'contract_role_travel_willingness_option_id'
 
