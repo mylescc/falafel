a789bd54b17d7f42927258742206e8abe5753977
Graham Pengelly <graham@three-tier.com>
2015-12-22 12:41:16 +0000

    Prevent caching of validate_profile service response

--- app/controllers/validate_profiles_controller.rb
+++ app/controllers/validate_profiles_controller.rb
@@ -1,6 +1,7 @@
 class ValidateProfilesController < ApplicationController
 
   def show
+    set_no_cache_headers
     @profile_validation_summary = ProfileValidator.new(current_user).validate
     respond_to do |format|
       format.json do
@@ -9,4 +10,11 @@ class ValidateProfilesController < ApplicationController
     end
   end
 
+  private
+
+  def set_no_cache_headers
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+  end
 end
