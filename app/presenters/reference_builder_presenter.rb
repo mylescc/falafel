08635763d020ba00782b7d3926eaf1d49c150ba7
Graham Pengelly <graham@three-tier.com>
2016-02-22 21:43:30 +0000

    Utilise STI in `ReferenceBuilderPresenter`
    We no longer need to make so many separate db calls since the switch to
    STI for `UserExperience`

--- app/presenters/reference_builder_presenter.rb
+++ app/presenters/reference_builder_presenter.rb
@@ -1,6 +1,5 @@
 class ReferenceBuilderPresenter
-  attr_reader :reference, :user, :applications, :industries, :infrastructures,
-    :languages, :management_skills, :methods, :roles
+  attr_reader :user_experiences
 
   def self.load(reference)
     ReferenceBuilderPresenter.new(reference)
@@ -9,18 +8,35 @@ class ReferenceBuilderPresenter
   def initialize(reference)
     @reference = reference
     @user = @reference.user
-    @applications = UserExperienceApplication.where(user_id: @user.id)
-    @industries = UserExperienceIndustry.where(user_id: @user.id)
-    @infrastructures = UserExperienceInfrastructure.where(user_id: @user.id)
-    @languages = UserExperienceLanguage.where(user_id: @user.id)
-    @management_skills = UserExperienceManagementSkill.where(user_id: @user.id)
-    @methods = UserExperienceMethod.where(user_id: @user.id)
-    @roles = UserExperienceRole.where(user_id: @user.id)
+    @user_experiences = UserExperience.where(user_id: @user.id)
   end
 
-  def all_experience
-    applications + industries + infrastructures + languages +
-      management_skills + methods + roles
+  def applications
+    user_experiences.select{ |exp| exp.is_a?(UserExperienceApplication) }
+  end
+
+  def industries
+    user_experiences.select{ |exp| exp.is_a?(UserExperienceIndustry) }
+  end
+
+  def infrastructures
+    user_experiences.select{ |exp| exp.is_a?(UserExperienceInfrastructure) }
+  end
+
+  def languages
+    user_experiences.select{ |exp| exp.is_a?(UserExperienceLanguage) }
+  end
+
+  def management_skills
+    user_experiences.select{ |exp| exp.is_a?(UserExperienceManagementSkill) }
+  end
+
+  def methods
+    user_experiences.select{ |exp| exp.is_a?(UserExperienceMethod) }
+  end
+
+  def roles
+    user_experiences.select{ |exp| exp.is_a?(UserExperienceRole) }
   end
 
   def partial_path(experience)
