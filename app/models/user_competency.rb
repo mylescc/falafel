676bde352023c76d1d7c90c38a126a5af4e82fe6
Graham Pengelly <graham@three-tier.com>
2016-03-17 18:40:38 +0000

    Extract `Competencies` from `UserCompetency`
    This enum is also required in the about to be added reference competency
    functionality

--- app/models/user_competency.rb
+++ app/models/user_competency.rb
@@ -1,10 +1,5 @@
 class UserCompetency < ActiveRecord::Base
-
-  enum competency: {'Verbal Communication' => 1, 'Written Communication' => 2, 'Customer Service' => 3,
-    'Problem Solving' => 4, 'Analytical' => 5, 'Working Under Pressure' => 6, 'Team Player' => 7,
-    'Strategic Thinker' => 8, 'Results Oriented' => 9, 'Planning' => 10, 'Stakeholder Engagement' => 11,
-    'Negotiation' => 12, 'Influencing' => 13, 'Leadership' => 14, 'People Management' => 15, 'Multi-Tasking' => 16,
-    'Salesmanship' => 17, 'Resilience' => 18}
+  include Competencies
 
   def self.unassigned_competencies(user_id)
     user_competencies = UserCompetency.where(user_id: user_id)
